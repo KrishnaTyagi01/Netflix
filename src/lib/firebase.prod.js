e04a1d651de5bda234812f6183c9077a537d6cc1
KrishnaTyagi01 <tyagikrishna38@gmail.com>
2021-01-09 18:15:27 +0530

    removing config files

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -9,13 +9,7 @@ import 'firebase/auth'
 //WE NEED CONFIG HERE
 const config = {
    // CONFIG FILES HERE
-   apiKey: "AIzaSyDE-2eiXVqtMijUtJ9UZpIhx1w19QFfxX8",
-   authDomain: "netflix-clone-c9e2f.firebaseapp.com",
-   databaseURL: "https://netflix-clone-c9e2f.firebaseio.com",
-   projectId: "netflix-clone-c9e2f",
-   storageBucket: "netflix-clone-c9e2f.appspot.com",
-   messagingSenderId: "626043573316",
-   appId: "1:626043573316:web:585153ab1a292e69601811"
+   
 }
 
 const firebase = Firebase.initializeApp(config);
